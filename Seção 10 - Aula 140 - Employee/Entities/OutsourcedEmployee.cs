d7567165b7fa514a9cc521f326e3751ce1571ddb
Diogo Brandon <d_bb2001@hotmail.com>
2022-07-21 22:46:15 -0300

    Entities

---
+++ Seção 10 - Aula 140 - Employee/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeAndOutsourcedEmployee.Entities
+{
+    class OutsourcedEmployee: Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(double additionalCharge)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge;
+        }
+    }
+}
