01e7a1a53f6066d1d260701d39ea1992469136f1
Diogo Brandon <d_bb2001@hotmail.com>
2022-07-26 20:34:58 -0300

    Chess Exceptions Update

---
+++ Seção 12 - Chess/BoardEntities/Piece.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardEntities
+{
+    internal class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int NumMovements { get; protected set; }
+        public Board Board { get; protected set; }
+
+        public Piece()
+        {
+        }
+
+        public Piece(Board board, Color color)
+        {
+            Position = null;
+            Board = board;
+            Color = color;
+            NumMovements = 0;
+        }
+    }
+}
--- Seção 12 - Chess/Program.cs
+++ Seção 12 - Chess/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Chess
+﻿using BoardEntities;
+using ChessPieces;
+using BoardExceptions;
+
+namespace Chess
 {
+   
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Board board = new Board(8, 8);
+
+                board.PutPiece(new Tower(board, Color.Red), new Position(1, 4));
+                board.PutPiece(new Tower(board, Color.Red), new Position(0, 7));
+                board.PutPiece(new King(board, Color.Red), new Position(0, 0));
+                board.PutPiece(new Tower(board, Color.Blue), new Position(7, 2));
+                board.PutPiece(new King(board, Color.Blue), new Position(6, 0));
+
+                Screen.ShowBoard(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            Console.ReadLine();
         }
     }
 }
